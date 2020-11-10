19a5f166e623ec4ce17114fe3211d893e43cae89
Riccardo Ferrari <richi.f93@gmail.com>
2020-11-10 14:05:10 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var firstName = prompt("Come ti chiami?");
+var secondName = prompt("Qual è il tuo cognome?");
+var yourColor = prompt("Qual è il tuo coolore preferito?");
+
+document.getElementById('name').innerHTML = firstName;
+document.getElementById('lastname').innerHTML = secondName;
+document.getElementById('color').innerHTML = yourColor;
